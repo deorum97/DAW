81c3a7ae39c4835e9cdeaf66b552300609265b17
Vespertino <Vespertino@C04PC07.informatica.comercio>
2025-11-03 21:38:47 +0100

    inicio actividad 17

---
+++ DWES/ud2/formulario2/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form method="get" action="autenticacion.php">
+        <label for="usuario">Usuario
+            <input type="text">
+        </label>
+        <label for="contraseña">Contraseña
+            <input type="password">
+        </label>
+        <input type="submit" value="enviar">
+    </form>
+</body>
+</html>
