8ba30d16954727359c2594facb4e775ca63619c5
Vespertino <Vespertino@C04PC07.informatica.comercio>
2025-11-05 21:31:58 +0100

    formulario2

--- DWES/ud2/formulario2/index.php
+++ DWES/ud2/formulario2/index.php
@@ -1,18 +1,27 @@
+<?php
+    session_start();
+    if(isset($_SESSION["usuario"])){
+        echo $_SESSION["usuario"];
+        header("Location:calculos.php");
+    }
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Logueo</title>
 </head>
 <body>
     <form method="get" action="autenticacion.php">
-        <label for="usuario">Usuario
-            <input type="text">
+        <label for="usuario">Usuario:
+            <input type="text" name="usuario">
         </label>
-        <label for="contraseña">Contraseña
-            <input type="password">
+        <br>
+        <label for="contraseña">Contraseña:
+            <input type="password" name="clave">
         </label>
+        <br>
         <input type="submit" value="enviar">
     </form>
 </body>
